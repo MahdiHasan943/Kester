19516379696ceda03737f956e5078960b6ba5bfa
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-06-14 15:39:16 +0600

    complete sum functions

--- soccer.js
+++ soccer.js
@@ -9,9 +9,10 @@ function displayPlayer(selectPlayer){
         const name = playerArray[i];
         // Create tr element
         const tr = document.createElement('tr');
+     
         tr.innerHTML = `
-            <th class="pr-5">${i + 1}</th>
-            <td>${name}</td>
+            <th class="mx-auto pr-5">${i + 1}</th>
+            <td class='mx-auto'>${name}</td>
         `
         tableBody.appendChild(tr);
     }
@@ -32,4 +33,42 @@ function selectPlayer(element){
         alert('Sorry, you have already added 5 players 😶');
         return;
     }
+};
+const takeInputValue = (id) => {
+    const inputValue = document.getElementById(id);
+    const inputValueString = parseFloat(inputValue.value)
+    inputValue.value = '';
+    return inputValueString;
 }
+const TextValue = (id,money) => {
+    const TextFeild = document.getElementById(id);
+    const newTextFeild = parseFloat(TextFeild.innerText);
+    const currentTextFeild = newTextFeild + money;
+    if (currentTextFeild) {
+        return  TextFeild.innerText =currentTextFeild;  
+
+    }
+}
+
+
+document.getElementById('calculate').addEventListener('click', function () {
+    const playersCosts = takeInputValue('perPlayer')
+   
+    // console.log(playersCosts * playerArray.length);
+    const playerCostSum = playersCosts * playerArray.length;
+    // console.log(typeof playerCostSum);
+   TextValue('playerTotalCost',playerCostSum)
+    
+   
+
+})
+
+document.getElementById('total').addEventListener('click', function () {
+     const coach =takeInputValue('coach')
+    const manager = takeInputValue('manager')
+    const playerCost = document.getElementById('playerTotalCost');
+    const playerString = parseFloat(playerCost.innerText);
+    const sub = coach + manager+playerString;
+    TextValue('totalCost',sub)
+
+})
